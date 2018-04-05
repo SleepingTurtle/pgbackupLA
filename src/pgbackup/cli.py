805934495a9320374a77936715b76f56fb40c5fd
= <joshcharlestechie@gmail.com>
2018-04-05 00:43:35 +0000

    added validation of driver

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,5 +1,23 @@
-from argparse immport ArgumentParser
+from argparse import ArgumentParser, Action
 
-def create_parse():
+known_drivers = ['local', 's3']
+
+class DriverAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        driver, destination = values
+        if driver.lower not in known_drivers:
+            parser.error("Unknown  driver. Available drivers are 'local' and 's3'")
+        namespace.driver = driver.lower()
+        namespace.destination = destination
+
+
+def create_parser():
     parser = ArgumentParser()
+    parser.add_argument('url', help="URL of the PostgreSQL database to backup")
+    parser.add_argument('--driver',
+            help="How and where to store backup",
+            nargs=2,
+            action=DriverAction,
+            required=True
+            )
     return parser
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -21,13 +21,33 @@ def test_parser_with_driver():
     with pytest.raises(SystemExit):
         parser.parse_args([url, "--driver", "local"])
 
+def test_parser_with_unknow_driver():
+    """
+    The parser will exit if the driver name is unkinow.
+    """
+
+    parser = cli.create_parser()
+
+    with pytest.raises(SystemExit):
+        parser.parse_args([url, "--driver", "azure", "destination"])
+
+def test_parser_with_known_drivers():
+    """
+    The parser will not exit if the driver name is known.
+    """
+
+    parser = cli.create_parser()
+
+    for driver in ['local', 's3']:
+        assert parser.parse_args([url, "--driver", driver, "destination"])
+
 def test_parser_with_driver_and_destination():
     """
     The parser will not exit if it receives a driver and destination
     """
     parser = cli.create_parser()
 
-    arg = parser.parse_args([url, "--driver", "local", "/some/path"])
+    args = parser.parse_args([url, "--driver", "local", "/some/path"])
 
     assert args.driver == "local"
     assert args.destination == "/some/path"
